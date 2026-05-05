6f355ff86a5f38bc6ccb281fdfebf1076c4e810b
fmg2099 <78172934+fmg2099@users.noreply.github.com>
2026-05-04 18:36:47 -0600

    Refactor MIFARE reading to support specific AID
    Updated the example to read ISO14443A cards with a specific AID. Removed old MIFARE reading logic.

--- firmware/readMifare.ino
+++ firmware/readMifare.ino
@@ -1,23 +1,6 @@
 /**************************************************************************/
 /*! 	
-	Ejemplo modificado de lectura de tarjetas MIFARE
-		This example will wait for any ISO14443A card or tag, and
-		depending on the size of the UID will attempt to read from it.
-	 
-		If the card has a 4-byte UID it is probably a Mifare
-		Classic card, and the following steps are taken:
-	 
-		- Authenticate block 4 (the first block of Sector 1) using
-			the default KEYA of 0XFF 0XFF 0XFF 0XFF 0XFF 0XFF
-		- If authentication succeeds, we can then read any of the
-			4 blocks in that sector (though only block 4 is read here)
-	 
-		If the card has a 7-byte UID it is probably a Mifare
-		Ultralight card, and the 4 byte pages can be read directly.
-		Page 4 is read by default since this is the first 'general-
-		purpose' page on the tags.
-
-		To enable debug message, define DEBUG in PN532/PN532_debug.h
+	Ejemplo de lectura de tarjetas ISO14443A con un AID específico
 */
 /**************************************************************************/
 
@@ -89,75 +72,36 @@ void setup(void) {
 
 void loop(void) {
 	uint8_t success;
-	uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };	// Buffer to store the returned UID
-	uint8_t uidLength;												// Length of the UID (4 or 7 bytes depending on ISO14443A card type)
-		
-	// Wait for an ISO14443A type cards (Mifare, etc.).	When one is found
-	// 'uid' will be populated with the UID, and uidLength will indicate
-	// if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
-	success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
-	if (success) {
-		Serial.print("	UID Value: ");
-		nfc.PrintHex(uid, uidLength);
-		//probablemente sea un fono
-		if (uidLength == 4)
-		{
-			Serial.print("UID lenght is 4");
-			uint8_t keya[6] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
-			success = nfc.mifareclassic_AuthenticateBlock(uid, uidLength, 4, 0, keya);
-			if (success)
-			{
-				uint8_t data[16];
-				success = nfc.mifareclassic_ReadDataBlock(4, data);
-				if (success)
-				{
-					// Data seems to have been read ... spit it out
-					Serial.println("Reading Block 4:");
-					nfc.PrintHexChar(data, 16);
-					Serial.println("");
-					// Wait a bit before reading the card again
-					delay(1000);
-				}
-				else
-				{
-					Serial.println("error reading block data");
-				}
-			}
-			else
-			{
-				Serial.println("authentication failed");
-				delay(1000);
-			}
-		}
-		if (uidLength == 7)
-		{
-			// We probably have a Mifare Ultralight card ...
-			Serial.println("Seems to be a Mifare Ultralight tag (7 byte UID)");
-		
-			// Try to read the first general-purpose user page (#4)
-			Serial.println("Reading page 4");
-			uint8_t data[32];
-			success = nfc.mifareultralight_ReadPage (4, data);
-			if (success)
-			{
-				// Data seems to have been read ... spit it out
-				nfc.PrintHexChar(data, 4);
-				Serial.println("");
-				rgb(0,1,0);
-				delay(250);
-				rgb(0,0,0);
-				// Wait a bit before reading the card again
-				delay(750);
-			}
-			else
-			{
-				Serial.println("Ooops ... unable to read the requested page!?");
-				rgb(1,0,0);
-				delay(250);
-				rgb(0,0,0);
-			}
-		}
-	}	
+  uint8_t response[255];
+  uint8_t responseLength;
+  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, response, &responseLength);
+  if (success) {
+    Serial.println("Found a device!");
+    // 2. Select the Application ID F2233445566
+    // Structure: CLA INS P1 P2 Lc [DATA] Length
+    uint8_t selectApp[] = { 
+        0x00, 0xA4, 0x04, 0x00, 0x06, 
+        0xF2, 0x23, 0x34, 0x45, 0x56, 0x60
+    };
+    uint8_t relayResponse[64];
+    uint8_t relayResponseLength = 64;
+
+    success = nfc.inDataExchange(selectApp, sizeof(selectApp), relayResponse, &relayResponseLength);
+
+    if (success) {
+      Serial.print("Response from Android: ");
+      nfc.PrintHexChar(relayResponse, relayResponseLength);
+      
+      // If Android returns 0x90 0x00, it means the connection is established!
+      if (relayResponse[relayResponseLength-2] == 0x90) {
+          Serial.println("Handshake Success!");
+      }
+    } else {
+      Serial.println("Failed to select AID. Is the app in the foreground?");
+    }
+
+
+    delay(1000);
+  }
 	
 }
-
